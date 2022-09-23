5488660ef5f672aa66b297e3cc43b9df551faf79
HugoCaridad <113972682+HugoCaridad@users.noreply.github.com>
2022-09-23 10:55:19 +0200

    Add files via upload

---
+++ while contador.py
@@ -0,0 +1,12 @@
+#realizar un contador hasta 10 usando while
+
+contador = 0
+termina = True
+while(termina):
+    print(contador)
+    contador = contador + 1
+    
+    if(contador == 11):
+        termina = False
+    
+print("Fin")
