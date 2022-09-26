849b26bd1bdbead18dd6603954c0395181b75312
HugoCaridad <113972682+HugoCaridad@users.noreply.github.com>
2022-09-26 12:15:14 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+#Realizar una funcion que tenga dos parametros de entrada y que la suma de los dos numeros
+
+#probar valores (3,4)
+def suma(x , y):
+    res = x + y
+    return res
+
+print("El resultado de la suma es: ", suma(3,4))
