824e97320ae5430598d280f6b08957aab238a60a
HugoCaridad <113972682+HugoCaridad@users.noreply.github.com>
2022-10-04 09:40:21 +0200

    Add files via upload

---
+++ sumatotal.py
@@ -0,0 +1,23 @@
+#Devolver la suma de los diez primeros números naturales
+
+num = 0
+index = 0
+
+while(index <= 10):
+    num += index
+    index += 1
+    
+print("El total es: ", num)
+
+#Devolver la suma de los n primeros numeros naturales
+def suma_total(n):
+        contador = 0
+        acumulador = 0
+        while(contador < n):
+            contador += 1
+            acumulador += contadoresPrimo
+        
+    print(n)
+
+num = int(input("Dime un número: "))
+suma_total(num)
